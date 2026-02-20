02f024fc53096c7ddd5c81e14e3a81399fa399f6
Lucian Behind The Scenes <silviulucian@gmail.com>
2026-02-20 12:37:57 +0200

    fixup

--- resources/views/components/docs/toc.blade.php
+++ resources/views/components/docs/toc.blade.php
@@ -15,12 +15,6 @@
     @endforeach
 
     <flux:navlist.group heading="Index">
-        <flux:navlist.item href="{{ route('command-index') }}" wire:navigate :current="$currentPath === 'command-index'">
-            <div class="flex items-center gap-2">
-                <span>Command Index</span>
-
-                <flux:icon.square-arrow-out-up-right class="size-3" />
-            </div>
-        </flux:navlist.item>
+        <flux:navlist.item href="{{ route('command-index') }}" wire:navigate :current="$currentPath === 'command-index'">Command Index</flux:navlist.item>
     </flux:navlist.group>
 </flux:navlist>
